0fcc9ab5c68b5dbd83dd08dc912a666990660a47
Renato <renatomp.rmp@gmail.com>
2020-08-28 11:25:54 -0300

    ContaClick
    App desenvolvido para portifolio

---
+++ src/screens/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { View, Text, Button } from 'react-native';
+
+import styles from '../styles/styles';
+
+export default ({ initial = 0, add = 1, decrease = 1 }) => {
+  const [number, setNumber] = useState(initial)
+
+  const increment = () => setNumber(number + add)
+  const subtract = () => setNumber(number - decrease)
+  const zero = () => setNumber(0)
+  const saveList = () => setList([...list, number])
+
+  const [fontSize, setFontSize] = useState(15)
+  const changeFontSize = () => fontSize <=44 && setFontSize(fontSize +2 )
+
+  const [list, setList] = useState([])
+
+  return (
+    <View style={styles.app}>
+      <Text style={styles.title}>Seja Bem Vindo ao ContaClick</Text >
+
+      <View style={{ flexDirection: 'row', width: 300, justifyContent: 'space-around', alignItems: 'center' }}>
+	    <Button title="Subtrair" onPress={subtract} color="#A20A20"></Button>
+	    <Text style={{ fontSize }}>{number}</Text>
+	    <Button title="Somar" onPress={increment} color = "#080"></Button>
+      </View>
+      
+      <View style={{ flexDirection: 'row', width: 350, justifyContent: 'space-around', alignItems: 'center' }}>
+      <Button title="Zerar" onPress={zero}></Button>
+      <Button title="Fonte" onPress={changeFontSize}></Button>
+      </View>
+
+
+      <Button title="Salvar" onPress={saveList}></Button>
+      <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+      {list.map(n => (<Text style={{ marginRight: 7 }}>{n}</Text>))}
+      </View>
+    </View>
+  )
+}
---
+++ src/styles/styles.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  app: {
+    backgroundColor: '#ADF',
+    flex: 1,
+    justifyContent: 'space-between',
+    padding: 50,
+    alignItems: 'center',
+  },
+  title: {
+    marginTop: 16,
+    paddingVertical: 8,
+    borderWidth: 3,
+    borderColor: "#20232a",
+    borderRadius: 7,
+    backgroundColor: "#50dafb",
+    color: "#20232a",
+    textAlign: "center",
+    fontSize: 35,
+    fontWeight: "bold"
+  },
+});
